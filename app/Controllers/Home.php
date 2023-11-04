7523ccf2de5298cab63511c8b15466ede7ba103d
Marcin Snoch <marcin.msx@gmail.com>
2023-11-04 21:55:17 +0100

    Poprawki i upgarde do CI 4.4.3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index()
     {
+        if ($this->request->getGet('run') == 3) {
+            alertDefault('Alert message', ['title' => 'Alert title', 'icon' => 'fas fa-envelope fa-lg', 'delay' => 7000]);
+//            alertError('Alert message', ['delay' => 1000000]);
+            return redirect()->to('home');
+        }
         return $this->twig->display('home/index');
     }
 }
--- app/Helpers/alerts_helper.php
+++ app/Helpers/alerts_helper.php
@@ -1,29 +1,36 @@
 <?php
 
-if (!function_exists('alertSuccess')) {
-    function alertSuccess($msg, $title = null, $options = null)
+if (!function_exists('alertDefault')) {
+    function alertDefault($msg = 'Alert default', $options = [])
     {
-        session()->setFlashdata('alert', ['type' => 'success', 'msg' => $msg, 'title' => $title, 'options' => json_encode($options)]);
+        session()->setFlashdata('alert', ['type' => 'default', 'msg' => $msg, 'options' => json_encode($options)]);
+    }
+}
+
+if (!function_exists('alertSuccess')) {
+    function alertSuccess($msg = 'Alert success', $options = [])
+    {
+        session()->setFlashdata('alert', ['type' => 'success', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
 
 if (!function_exists('alertInfo')) {
-    function alertInfo($msg, $title = null, $options = null)
+    function alertInfo($msg = 'Alert info', $options = [])
     {
-        session()->setFlashdata('alert', ['type' => 'info', 'msg' => $msg, 'title' => $title, 'options' => json_encode($options)]);
+        session()->setFlashdata('alert', ['type' => 'info', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
 
 if (!function_exists('alertWarning')) {
-    function alertWarning($msg, $title = null, $options = null)
+    function alertWarning($msg = 'Alert warning', $options = [])
     {
-        session()->setFlashdata('alert', ['type' => 'warning', 'msg' => $msg, 'title' => $title, 'options' => json_encode($options)]);
+        session()->setFlashdata('alert', ['type' => 'warning', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
 
 if (!function_exists('alertError')) {
-    function alertError($msg, $title = null, $options = null)
+    function alertError($msg = 'Alert error', $options = [])
     {
-        session()->setFlashdata('alert', ['type' => 'error', 'msg' => $msg, 'title' => $title, 'options' => json_encode($options)]);
+        session()->setFlashdata('alert', ['type' => 'danger', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
