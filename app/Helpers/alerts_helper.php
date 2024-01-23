c7251c1b512d23f500ca918b569c27c60cdd01a5
Marcin Snoch <marcin.msx@gmail.com>
2024-01-23 18:40:43 +0100

    Poprawka flash alerts

--- app/Helpers/alerts_helper.php
+++ app/Helpers/alerts_helper.php
@@ -3,6 +3,7 @@
 if (!function_exists('alertDefault')) {
     function alertDefault($msg = 'Alert default', $options = [])
     {
+        $options['title'] = 'Message';
         session()->setFlashdata('alert', ['type' => 'default', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
@@ -10,6 +11,7 @@ if (!function_exists('alertDefault')) {
 if (!function_exists('alertSuccess')) {
     function alertSuccess($msg = 'Alert success', $options = [])
     {
+        $options['title'] = 'Success';
         session()->setFlashdata('alert', ['type' => 'success', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
@@ -17,6 +19,7 @@ if (!function_exists('alertSuccess')) {
 if (!function_exists('alertInfo')) {
     function alertInfo($msg = 'Alert info', $options = [])
     {
+        $options['title'] = 'Info';
         session()->setFlashdata('alert', ['type' => 'info', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
@@ -24,6 +27,7 @@ if (!function_exists('alertInfo')) {
 if (!function_exists('alertWarning')) {
     function alertWarning($msg = 'Alert warning', $options = [])
     {
+        $options['title'] = 'Warning';
         session()->setFlashdata('alert', ['type' => 'warning', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
@@ -31,6 +35,7 @@ if (!function_exists('alertWarning')) {
 if (!function_exists('alertError')) {
     function alertError($msg = 'Alert error', $options = [])
     {
+        $options['title'] = 'Error!';
         session()->setFlashdata('alert', ['type' => 'danger', 'msg' => $msg, 'options' => json_encode($options)]);
     }
 }
